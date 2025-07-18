f164ef160f540d49180ae4a0ea1924dd66891c5b
Carlotta-Montelli <carlotta.montelli.0201@gmail.com>
2025-07-19 04:38:22 +0700

    Update Git-Config.sh

--- Common/Git-Config.sh
+++ Common/Git-Config.sh
@@ -18,5 +18,5 @@ echo "==========================================================================
 echo "Add Git Config"
 echo "=============================================================================="
 
-git config --global user.name "mizuenaAlt"
-git config --global user.email "enashinonome264@gmail.com"
+git config --global user.name "Carlotta-Montelli"
+git config --global user.email "carlotta.montelli.0201@gmail.com"
